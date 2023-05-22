348d2653f41253fe3c08e6aa6d262951cff19542
mrudnev <pleasegivemeonechance@mail.ru>
2023-05-22 17:26:22 +0300

    messenger

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+const EmptyState = () => {
+    return (
+        <div
+            className="
+        px-4
+        px-10
+        sm:px-6
+        lg:px-8
+        h-full
+        flex
+        justify-center
+        items-center
+        bg-gray-100
+        "
+        >1
+        </div>
+    );
+}
+
+export default EmptyState;
